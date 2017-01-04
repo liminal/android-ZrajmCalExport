ead794fbe383fc7f1ce9f271e9c12a40c9c14db9
Karl Åström <karl.astrom@gmail.com>
2017-01-04 18:16:15 +0100

    add very useful comment

--- app/src/main/java/se/lightside/zrajmcalexport/CalendarWriter.java
+++ app/src/main/java/se/lightside/zrajmcalexport/CalendarWriter.java
@@ -6,7 +6,9 @@ import android.support.v4.provider.DocumentFile;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -22,18 +24,38 @@ public class CalendarWriter {
 
     public void writeCalendar(final DocumentFile tree, final CalendarModel calendar) {
         try {
-            final String filename = URLEncoder.encode(calendar.getName(), "UTF-8") + ".txt";
+            final String filename = filenameFor(calendar);
             Timber.v("CALENDAR: %s", filename);
             final DocumentFile         file = tree.createFile("text/plain", filename);
             final ParcelFileDescriptor pfd  = mContentResolver.openFileDescriptor(file.getUri(), "w");
-            final FileOutputStream     fos  = new FileOutputStream(pfd.getFileDescriptor());
-            for (Event event : calendar.getEventList()) {
-                fos.write(event.toString().getBytes());
+            writeToDisk(pfd, calendar.getEventList());
+            if (pfd != null) {
+                pfd.close();
             }
-            fos.close();
-            pfd.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String filenameFor(CalendarModel calendar) throws UnsupportedEncodingException {
+        return URLEncoder.encode(calendar.getName(), "UTF-8") + ".txt";
+    }
+
+    private void writeToDisk(final ParcelFileDescriptor pfd, final List<Event> eventList) {
+        if (pfd == null) return;
+        try (FileOutputStream     fos  = new FileOutputStream(pfd.getFileDescriptor())) {
+            for (Event event : eventList) {
+                fos.write(outputEvent(event));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Make this output something useful... like vEvent
+     */
+    private byte[] outputEvent(Event event) {
+        return event.toString().getBytes();
     }
 }
